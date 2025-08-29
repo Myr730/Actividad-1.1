16289bdac7af32cfafda3f52bb423c9e9067bfcb
myrka santiago <A01735601@tec.mx>
2025-08-29 14:45:03 -0600

    2

--- OneDrive/Documentos/Pogramacion/c++/dos.cpp
+++ OneDrive/Documentos/Pogramacion/c++/dos.cpp
@@ -1,7 +1,7 @@
 
  // ordenamiento parcial (QuickSelect)
  // Autor: Myrka Santiago Lopez A01735601
- // fecha: 2023-11-15
+ // fecha: 2025-08-29
 
 #include <iostream>
 #include <vector>
@@ -19,3 +19,29 @@ using namespace std::chrono;
  //param arr - arreglo de números enteros
  //param left - indice izquierdo del subarreglo
  //param right - indice derecho del subarreglo  
+ //param pivotIndex - indice del pivote seleccionado
+ 
+int particionar(vector<int>& arr, int left, int right, int pivotIndex) {
+    int valorPivote = arr[pivotIndex]; // almacenar el valor del pivote para comparaciones
+    
+    swap(arr[pivotIndex], arr[right]); // para el pivote al final 
+
+    // indice para seguir la posición donde colocar elementos menores
+    int indiceAlmacenamiento = left;
+    
+    // comparar todos los números con el pivote
+    for (int i = left; i < right; i++) {
+        if (arr[i] < valorPivote) {
+            // Mover números menores a la izquierda
+            swap(arr[i], arr[indiceAlmacenamiento]);
+            indiceAlmacenamiento++;
+        }
+    }
+    
+    // Poner el pivote en su posición correcta (entre menores y mayores)
+    swap(arr[right], arr[indiceAlmacenamiento]);
+    
+    // Devolver la posición final del pivote
+    return indiceAlmacenamiento;
+}
+
